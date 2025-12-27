3bb8591ec4fc69f93d14610061222297f99097c4
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2025-12-27 12:14:15 +0530

    Update MainActivity.java

--- app/src/main/java/com/google/android/youtube/pro/MainActivity.java
+++ app/src/main/java/com/google/android/youtube/pro/MainActivity.java
@@ -195,9 +195,9 @@ public class MainActivity extends Activity {
       public void onPageFinished(WebView p1, String url) {
 
         web.evaluateJavascript("if (window.trustedTypes && window.trustedTypes.createPolicy && !window.trustedTypes.defaultPolicy) {window.trustedTypes.createPolicy('default', {createHTML: (string) => string,createScriptURL: string => string, createScript: string => string, });}",null);
-        web.evaluateJavascript("(function () { var script = document.createElement('script'); script.src='https://cdn.jsdelivr.net/gh/SP-Mods-WA/Yt@latest/scripts/script.js'; document.body.appendChild(script);  })();",null);
-web.evaluateJavascript("(function () { var script = document.createElement('script'); script.src='https://cdn.jsdelivr.net/gh/SP-Mods-WA/Yt@latest/scripts/bgplay.js'; document.body.appendChild(script);  })();",null);
-web.evaluateJavascript("(function () { var script = document.createElement('script');script.type='module';script.src='https://cdn.jsdelivr.net/gh/SP-Mods-WA/Yt@latest/scripts/innertube.js'; document.body.appendChild(script);  })();",null);
+        web.evaluateJavascript("(function () { var script = document.createElement('script'); script.src='https://youtube.com/ytpro_cdn/npm/ytpro/script.js'; document.body.appendChild(script);  })();",null);
+web.evaluateJavascript("(function () { var script = document.createElement('script'); script.src='https://youtube.com/ytpro_cdn/npm/ytpro/bgplay.js'; document.body.appendChild(script);  })();",null);
+web.evaluateJavascript("(function () { var script = document.createElement('script');script.type='module';script.src='https://youtube.com/ytpro_cdn/npm/ytpro/innertube.js'; document.body.appendChild(script);  })();",null);
 
 
 
