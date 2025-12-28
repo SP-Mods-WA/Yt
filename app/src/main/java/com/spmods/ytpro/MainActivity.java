a67913ea143522e3e3f30ff4e9550166ae57e0b2
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2025-12-28 23:27:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/spmods/ytpro/MainActivity.java
+++ app/src/main/java/com/spmods/ytpro/MainActivity.java
@@ -3,6 +3,7 @@ package com.spmods.ytpro;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +18,30 @@ public class MainActivity extends AppCompatActivity {
         // Initialize NativeSec
         nativeSec = new NativeSec();
 
-        // Log native values (no TextView needed)
-        Log.d(TAG, "Native String: " + nativeSec.stringFromJNI());
-        Log.d(TAG, "YtPro URL: " + nativeSec.getYtproUrl());
-        Log.d(TAG, "CDN URL: " + nativeSec.getCdnUrl());
-        
-        // Your WebView or other UI setup here...
+        // Find TextView
+        TextView sampleText = findViewById(R.id.sample_text);
+        sampleText.setText(nativeSec.stringFromJNI());
+
+        // Get all URLs from native
+        String ytproUrl = nativeSec.getYtproUrl();
+        String innertubeJs = nativeSec.getInnertubeJs();
+        String cdnUrl = nativeSec.getCdnUrl();
+        String innertubeScriptUrl = nativeSec.getInnertubeScriptUrl();
+        String bgplayJs = nativeSec.getBgplayJs();
+        String bgplayScriptUrl = nativeSec.getBgplayScriptUrl();
+        String scriptJs = nativeSec.getScriptJs();
+        String scriptJsUrl = nativeSec.getScriptJsUrl();
+        String youtubeBaseUrl = nativeSec.getYoutubeBaseUrl();
+
+        // Log all URLs
+        Log.d(TAG, "YtPro URL: " + ytproUrl);
+        Log.d(TAG, "Innertube JS: " + innertubeJs);
+        Log.d(TAG, "CDN URL: " + cdnUrl);
+        Log.d(TAG, "Innertube Script URL: " + innertubeScriptUrl);
+        Log.d(TAG, "Bgplay JS: " + bgplayJs);
+        Log.d(TAG, "Bgplay Script URL: " + bgplayScriptUrl);
+        Log.d(TAG, "Script JS: " + scriptJs);
+        Log.d(TAG, "Script JS URL: " + scriptJsUrl);
+        Log.d(TAG, "YouTube Base URL: " + youtubeBaseUrl);
     }
 }
