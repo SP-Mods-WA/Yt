69318b725e3be2255506dec7845a076344f1e673
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2025-12-28 21:17:20 +0530

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,68 +1,91 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
-// 🔐 XOR Decryption
-std::string xorDecrypt(const std::string& encrypted, const std::string& key) {
-    std::string decrypted;
-    size_t keyLen = key.length();
+#define LOG_TAG "YTSec-Native"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+// XOR encryption key (hidden in native code)
+static const char ENCRYPTION_KEY[] = "YTPro2025SecureKey";
+static const int KEY_LENGTH = 18;
+
+/**
+ * Simple XOR encryption/decryption
+ * XOR is symmetric - same function for encrypt and decrypt
+ */
+std::string xorCrypt(const std::string& input) {
+    std::string output = input;
     
-    for (size_t i = 0; i < encrypted.length(); i++) {
-        decrypted += encrypted[i] ^ key[i % keyLen];
+    for (size_t i = 0; i < input.length(); i++) {
+        output[i] = input[i] ^ ENCRYPTION_KEY[i % KEY_LENGTH];
     }
     
-    return decrypted;
+    return output;
 }
 
-// 🔐 Encrypted URLs (Raw bytes)
-const std::string ENC_YOUTUBE_BASE = "\x16\x13\x19\x04\x04\x1e\x11\x15\x02\x11\x1c\x0b\x07\x16\x1e\x15\x17\x14\x1f\x02\x1e\x1d\x1b\x0d\x06\x16\x0f";
-const std::string ENC_INNERTUBE = "\x16\x13\x19\x04\x04\x1e\x11\x15\x02\x11\x1c\x08\x1e\x10\x1e\x0b\x16\x19\x07\x0f\x10\x0f\x1a\x1b\x0e\x03\x1e\x12\x15\x10\x1c\x1a\x1c\x02\x1e\x1f\x12\x1f\x1a\x1b\x06\x10\x0f\x1d\x18\x0f\x19\x1e\x1c\x17\x0f\x1c\x0d\x1e\x1c\x15\x11\x16\x0f\x10\x02\x18\x16\x19\x16\x1c\x1f\x02\x1b\x1e\x15";
-const std::string ENC_BGPLAY = "\x16\x13\x19\x04\x04\x1e\x11\x15\x02\x11\x1c\x08\x1e\x10\x1e\x0b\x16\x19\x07\x0f\x10\x0f\x1a\x1b\x0e\x03\x1e\x12\x15\x10\x1c\x1a\x1c\x02\x1e\x1f\x12\x1f\x1a\x1b\x06\x10\x0f\x1d\x18\x0f\x19\x1e\x1c\x17\x0f\x1c\x0d\x1e\x1c\x15\x11\x16\x0f\x0e\x06\x1a\x16\x1a\x16\x0f\x1f\x02\x1b\x1e\x15";
-const std::string ENC_SCRIPT = "\x16\x13\x19\x04\x04\x1e\x11\x15\x02\x11\x1c\x08\x1e\x10\x1e\x0b\x16\x19\x07\x0f\x10\x0f\x1a\x1b\x0e\x03\x1e\x12\x15\x10\x1c\x1a\x1c\x02\x1e\x1f\x12\x1f\x1a\x1b\x06\x10\x0f\x1d\x18\x0f\x19\x1e\x1c\x17\x0f\x1c\x0d\x1e\x1c\x15\x11\x16\x0f\x10\x1b\x1a\x16\x1a\x16\x0f\x1f\x02\x1b\x1e";
-const std::string ENC_CDN_BASE = "\x1a\x0d\x06\x16\x1e\x07\x1a\x0b\x0f\x12\x19\x07\x0f\x10\x0f\x1a\x1b\x0e\x03\x1e\x12\x15\x10\x1c\x1a\x1c\x02\x1e\x1f\x12\x1f\x1a\x1b\x06\x10\x0f\x1d\x18\x0f\x19\x1e\x1c\x17\x0f\x1c\x0d\x1e\x1c\x15\x11\x16\x0f";
-const std::string ENC_ESM = "\x1a\x1b\x0f\x16\x0f\x1d\x11";
-const std::string ENC_API_URL = "\x16\x13\x19\x04\x04\x1e\x11\x15\x02\x11\x1c\x1a\x1a\x12\x1e\x06\x11\x1b\x0e\x03\x1e\x1d\x15\x1a\x1c\x18\x1c\x02\x1e\x1f\x12\x1f\x1a\x1b\x1d\x10\x1c\x1c\x1e\x11\x1b\x1e\x1b\x16\x1c\x1f\x15\x11\x16\x1a\x1e\x1f\x05";
-
-const std::string SECRET_KEY = "spmods";
-
-// 🔓 Native Methods
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getYouTubeUrl(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_YOUTUBE_BASE, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
+/**
+ * JNI Native Method: Encrypt
+ * Called from Java: NativeSec.enc(String)
+ */
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_spmods_ytpro_NativeSec_enc(JNIEnv *env, jclass clazz, jstring data) {
+    
+    // Null check
+    if (data == nullptr) {
+        LOGE("Encrypt: null input received");
+        return env->NewStringUTF("");
+    }
+    
+    // Get input string
+    const char* input = env->GetStringUTFChars(data, nullptr);
+    if (input == nullptr) {
+        LOGE("Encrypt: failed to get string chars");
+        return env->NewStringUTF("");
+    }
+    
+    // Encrypt
+    std::string encrypted = xorCrypt(std::string(input));
+    
+    // Release input
+    env->ReleaseStringUTFChars(data, input);
+    
+    LOGI("✅ Encrypted: %zu bytes", encrypted.length());
+    
+    // Return encrypted string
+    return env->NewStringUTF(encrypted.c_str());
 }
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getInnerTubeUrl(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_INNERTUBE, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getBgPlayUrl(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_BGPLAY, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getScriptUrl(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_SCRIPT, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getCdnBase(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_CDN_BASE, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getEsmDomain(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_ESM, SECRET_KEY);
-    return env->NewStringUTF(decrypted.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_spmods_ytpro_MainActivity_getApiUrl(JNIEnv* env, jobject) {
-    std::string decrypted = xorDecrypt(ENC_API_URL, SECRET_KEY);
+/**
+ * JNI Native Method: Decrypt
+ * Called from Java: NativeSec.dec(String)
+ */
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_spmods_ytpro_NativeSec_dec(JNIEnv *env, jclass clazz, jstring data) {
+    
+    // Null check
+    if (data == nullptr) {
+        LOGE("Decrypt: null input received");
+        return env->NewStringUTF("");
+    }
+    
+    // Get input string
+    const char* input = env->GetStringUTFChars(data, nullptr);
+    if (input == nullptr) {
+        LOGE("Decrypt: failed to get string chars");
+        return env->NewStringUTF("");
+    }
+    
+    // Decrypt (XOR is symmetric, same function)
+    std::string decrypted = xorCrypt(std::string(input));
+    
+    // Release input
+    env->ReleaseStringUTFChars(data, input);
+    
+    LOGI("✅ Decrypted: %zu bytes", decrypted.length());
+    
+    // Return decrypted string
     return env->NewStringUTF(decrypted.c_str());
 }
