04f18b3a7f84cd277ac77be76b4978b252df5065
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-02-12 15:32:20 +0530

    Update script.js

--- app/src/main/res/raw/script.js
+++ app/src/main/res/raw/script.js
@@ -743,7 +743,7 @@ ytpSetI.innerHTML+=`<br><b style='font-size:18px' >SPYT PRO</b>
 </svg>
 </button>
 <br>
-<button onclick="Android.oplink('spmodsofficial@gmail.com')">Report Bugs
+<button onclick="Android.oplink('mailto:spmodsofficial@gmail.com')">Report Bugs
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${isD ? "#ccc" : "#444"}" viewBox="0 0 16 16">
 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
 </svg>
