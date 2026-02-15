836e7070011afd896dc0624c5a4da2afe9c7ab04
SP-Mods-WA <94231157+SP-Mods-WA@users.noreply.github.com>
2026-02-15 11:48:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/spmods/ytpro/MainActivity.java
+++ app/src/main/java/com/spmods/ytpro/MainActivity.java
@@ -1326,7 +1326,7 @@ public class MainActivity extends Activity {
         });
     } else {
         rootView.post(() -> {
-            int statusBarHeight = getStatusBarHeight();
+            
             int navBarHeight = getNavigationBarHeight();
             applyInsets(statusBarHeight, navBarHeight);
         });
